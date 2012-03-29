9dae23de76a5efa487f859a8aed1734f97a1aa43
muzuiget <muzuiget@gmail.com>
2012-03-29 19:54:41 +0800

    add tudou support

--- flvcd_download_links.user.js
+++ flvcd_download_links.user.js
@@ -2,12 +2,13 @@
 // @name           Flvcd Download Links
 // @namespace      http://qixinglu.com
 // @description    替换在线视频的下载链接为 flvcd.com 的解析链接
-// @include        http://v.youku.com/v_show/*.html
-// @include        http://v.youku.com/v_playlist/*.html
+// @include        http://v.youku.com/v_show/*.html*
+// @include        http://v.youku.com/v_playlist/*.html*
+// @include        http://www.tudou.com/playlist/p/*.html*
 // ==/UserScript==
 
-function create_flvcd_url() {
-    return 'http://www.flvcd.com/parse.php?kw=' + encodeURIComponent(document.URL) + '&flag=&format=super';
+function create_flvcd_url(format) {
+    return 'http://www.flvcd.com/parse.php?kw=' + encodeURIComponent(document.URL) + '&flag=&format=' + format;
 }
 
 var sites = [
@@ -17,7 +18,17 @@ var sites = [
             var source_link_node = document.getElementById('fn_favodownload').lastElementChild;
             var new_link_node = document.createElement('a');
             new_link_node.text = '下载';
-            new_link_node.href = create_flvcd_url();
+            new_link_node.href = create_flvcd_url('super');
+            source_link_node.parentNode.replaceChild(new_link_node, source_link_node);
+        }
+    },
+    {
+        domain: 'tudou.com',
+        handler: function() {
+            var source_link_node = document.getElementById('download');
+            var new_link_node = document.createElement('a');
+            new_link_node.innerHTML = '<span class="ico"></span>下载';
+            new_link_node.href = create_flvcd_url('real');
             source_link_node.parentNode.replaceChild(new_link_node, source_link_node);
         }
     }
