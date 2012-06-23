abdaababeb4a6de78203573da374e74dc3ff4c76
muzuiget <muzuiget@gmail.com>
2012-06-23 16:50:10 +0800

    suport tudou new style

--- flvcd_download_links.user.js
+++ flvcd_download_links.user.js
@@ -4,22 +4,24 @@
 // @description    替换在线视频的下载链接为 flvcd.com 的解析链接
 // @include        http://v.youku.com/v_show/*.htm*
 // @include        http://v.youku.com/v_playlist/*.htm*
-// @include        http://www.tudou.com/playlist/p/*.htm*
-// @include        http://www.tudou.com/albumplay/*/*.htm*
+// @include        http://www.tudou.com/playlist/*.htm*
+// @include        http://www.tudou.com/albumplay/*.htm*
+// @include        http://www.tudou.com/programs/view/*
 // ==/UserScript==
 
 function create_flvcd_url(format) {
     return 'http://www.flvcd.com/parse.php?kw=' + encodeURIComponent(document.URL) + '&flag=&format=' + format;
 }
 
+var title = '用 Flvcd 下载视频';
 var sites = [
     {
         domain: 'youku.com',
         handler: function() {
-            var source_link_node = document.getElementById('fn_favodownload').lastElementChild;
-            var new_link_node = document.createElement('a');
-            new_link_node.text = '下载';
+            var source_link_node = document.getElementById('fn_download');
+            var new_link_node = source_link_node.cloneNode();
             new_link_node.href = create_flvcd_url('super');
+            new_link_node.title = title;
             source_link_node.parentNode.replaceChild(new_link_node, source_link_node);
         }
     },
@@ -27,9 +29,16 @@ var sites = [
         domain: 'tudou.com',
         handler: function() {
             var source_link_node = document.getElementById('download');
-            var new_link_node = document.createElement('a');
-            new_link_node.innerHTML = '<span class="ico"></span>下载';
+            var new_link_node = source_link_node.cloneNode();
+            if (source_link_node.parentNode.className === 'player_extra') {
+                new_link_node.id = 'flvcd-' + source_link_node.id;
+                new_link_node.classList.remove('disabled')
+            } else {
+                new_link_node.classList.remove('download');
+                new_link_node.style.width = '71px';
+            }
             new_link_node.href = create_flvcd_url('real');
+            new_link_node.title = title;
             source_link_node.parentNode.replaceChild(new_link_node, source_link_node);
         }
     }
