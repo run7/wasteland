9df071aa582da485c10b0400e580d7ac51e2c743
muzuiget <muzuiget@gmail.com>
2012-06-23 21:14:42 +0800

    add kuaichuan_display_full_filename.js

---
+++ kuaichuan_display_full_filename.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name        KuaiChuan - Display Full Filename
+// @namespace   qixinglu.com
+// @description 迅雷快传上的下载链接显完整文件名
+// @include     http://kuai.xunlei.com/d/*
+// ==/UserScript==
+
+var nodes = document.querySelectorAll('.c_2 a.file_name');
+var i, node;
+for (i = 0; i < nodes.length; i += 1) {
+    node = nodes[i];
+    node.textContent = node.title;
+}
+
+GM_addStyle('.c_2 { width: 620px !important;');
+
