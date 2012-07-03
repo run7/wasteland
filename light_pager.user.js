fd53f6b7ac4fce1d4249766be7e87acf43e5f87b
muzuiget <muzuiget@gmail.com>
2012-07-03 21:25:57 +0800

    refactor, can run as library

--- light_pager.user.js
+++ light_pager.user.js
@@ -7,98 +7,161 @@
 // @include        https://www.google.com/search?*
 // ==/UserScript==
 
-// load outside rule
-var RULE = JSON.parse(GM_getResourceText('rule'));
-var DEFAULT = RULE["default"];
-var SITES = RULE["sites"];
+/* eXtend querySelector */
+
+var querySelectorLast = function(element, selector) {
+    var nodes = element.querySelectorAll(selector);
+    if (nodes.length === 0) {
+        return null;
+    }
+    return nodes[nodes.length - 1];
+};
+
+var queryXSelectorAll = function(element, xselector) {
+    if (xselector.indexOf(':contains') !== -1) {
+        var parts = xselector.split(':contains');
+        var selector = parts[0];
+        var text = parts[1].slice(2, -2);
+
+        var nodes = element.querySelectorAll(selector);
+        var filtered_nodes = [];
+        var i, node;
+        for (i = 0; i < nodes.length; i += 1) {
+            node = nodes[i];
+            if (node.textContent.indexOf(text) !== -1) {
+                filtered_nodes.push(node);
+            }
+        }
+        return filtered_nodes;
+
+    } else if (xselector.indexOf(':eq') !== -1) {
+        var parts = xselector.split(':eq');
+        var selector = parts[0];
+        var text = parts[1].slice(2, -2);
+        var index = parseInt(text);
+
+        var nodes = element.querySelectorAll(selector);
+        return [nodes[index]];
+    }
+
+    return element.querySelectorAll(xselector);
+};
+
+var queryXSelector = function(element, xselector) {
+    var nodes = queryXSelectorAll(element, xselector);
+    return nodes[0];
+}
+
+var queryXSelectorLast = function(element, xselector) {
+    var nodes = queryXSelectorAll(element, selector);
+    if (nodes.length === 0) {
+        return null;
+    }
+    return nodes[nodes.length - 1];
+}
+
+/* ligher pager core */
 
 var light_pager = function(site) {
-    var mime_type = document.contentType + '; charset=' + document.characterSet;
-    var appending = false; // lock
 
-    if (site.height <= 1) {
-        if (window.scrollMaxY === 0) {
-            site.height = window.innerHeight * (1 - site.height);
+    var absolute_site_attrs = function() {
+        if (site.height <= 1) {
+            if (window.scrollMaxY === 0) {
+                site.height = window.innerHeight * (1 - site.height);
+            } else {
+                site.height = window.scrollMaxY * (1 - site.height);
+            }
+        }
+        if (site.count <= 0) {
+            site.count = 999; // not really endless
+        }
+    };
+
+    var get_document_mimetype = function() {
+        var mime_type = document.contentType;
+        mime_type += '; charset=' + document.characterSet;
+        return mime_type;
+    };
+
+    var reach_append_height = function() {
+        var current_height = window.scrollMaxY - window.scrollY;
+        return site.height > current_height;
+    };
+
+    var remove_elements = function(parent_node, selector) {
+        var nodes = parent_node.querySelectorAll(selector);
+        var i, node;
+        for (i = 0; i < nodes.length; i += 1) {
+            node = nodes[i];
+            node.parentNode.removeChild(node);
+        }
+    };
+
+    var get_next_append_url = function(the_document, selector) {
+        var next_url_node = queryXSelector(the_document, site.next);
+        if (next_url_node !== undefined) {
+            next_append_url = next_url_node.href;
         } else {
-            site.height = window.scrollMaxY * (1 - site.height);
+            next_append_url = null;
         }
-    }
-    if (site.count <= 0) {
-        site.count = 999; // not really endless
+        return next_append_url;
     }
 
-    site.appended_count = 0;
-    site.next_append_url = document.querySelector(site.next).href;
-
-    var querySelectorLast = function(selector) {
-        var nodes = document.querySelectorAll(selector);
-        if (nodes.length === 0) {
-            return null;
-        }
-        return nodes[nodes.length - 1];
+    var runtime = {
+        appending: false, // lock
+        mime_type: get_document_mimetype(),
+        appended_count: 0,
+        next_append_url: get_next_append_url(document, site.next)
     };
 
     var append_next_callback = function(response) {
         var temp_document = document.createElement('html');
         temp_document.innerHTML = response.responseText;
+        runtime.next_append_url = get_next_append_url(temp_document, site.next);
 
-        var temp_next_url_node = temp_document.querySelector(site.next);
-        if (temp_next_url_node !== null) {
-            site.next_append_url = temp_next_url_node.href;
-        } else {
-            site.next_append_url = null;
-        }
-
-        var temp_content_node = temp_document.querySelector(site.content).cloneNode();
+        var temp_content_nodes = temp_document.querySelectorAll(site.content);
         if (site.removes !== undefined) {
-            var temp_remove_nodes = temp_content_node.querySelectorAll(site.removes);
-            var i, remove_node;
-            for (i = 0; i < temp_remove_nodes.length; i += 1) {
-                remove_node = temp_remove_nodes[i];
-                remove_node.parentNode.removeChild(remove_node);
+            var i, temp_content_node;
+            for (i = 0; i < temp_content_nodes.length; i += 1) {
+                temp_content_node = temp_content_nodes[i];
+                remove_elements(temp_content_node, site.removes);
             }
         }
 
-        if (site.afters !== undefined) {
-            var potision_node = querySelectorLast(site.afters);
+        if (site.position !== undefined) {
+            var position_node = querySelectorLast(document, site.position);
         } else {
-            var potision_node = querySelectorLast(site.content);
+            var last_content_node = querySelectorLast(document, site.content);
+            var position_node = last_content_node.nextSibling;
         }
 
-        potision_node.parentNode.insertBefore(temp_content_node, potision_node.nextElementSibling);
-
-        if (site.befores !== undefined) {
-            var temp_before_nodes = temp_document.querySelectorAll(site.befores);
-            var i, before_node;
-            for (i = 0; i < temp_before_nodes.length; i += 1) {
-                before_node = temp_before_nodes[i].cloneNode();
-                temp_content_node.parentNode.insertBefore(before_node, temp_content_node);
-            }
+        var i, temp_content_node;
+        for (i = 0; i < temp_content_nodes.length; i += 1) {
+            temp_content_node = temp_content_nodes[i];
+            position_node.parentNode.insertBefore(temp_content_node,
+                                                  position_node);
         }
 
-        if (site.afters !== undefined) {
-            var temp_after_nodes = temp_document.querySelectorAll(site.afters);
-            var i, after_node;
-            for (i = 0; i < temp_after_nodes.length; i += 1) {
-                after_node = temp_after_nodes[i].cloneNode();
-                temp_content_node.parentNode.insertBefore(after_node, temp_content_node.nextElementSibling);
-            }
+        if (site.onAppended !== undefined) {
+            site.onAppended();
         }
 
-        site.appended_count += 1;
-        appending = false;
+        runtime.appended_count += 1;
+        runtime.appending = false;
     };
 
     var append_next = function() {
-        appending = true;
-        if (site.appended_count >= site.count || site.next_append_url === null) {
+        runtime.appending = true;
+        var has_remain_count = runtime.appended_count >= site.count;
+        var has_next_url = runtime.next_append_url === null;
+        if (has_remain_count || has_next_url) {
             stop_scroll_listener();
             return;
         }
         GM_xmlhttpRequest({
             method: "GET",
-            overrideMimeType: mime_type,
-            url: site.next_append_url,
+            overrideMimeType: runtime.mime_type,
+            url: runtime.next_append_url,
             onload: function(response) {
                 try {
                     append_next_callback(response);
@@ -109,13 +172,8 @@ var light_pager = function(site) {
         });
     };
 
-    var reach_append_height = function() {
-        var current_height = window.scrollMaxY - window.scrollY;
-        return site.height > current_height;
-    };
-
     var scroll_listener = function() {
-        if (appending) {
+        if (runtime.appending) {
             return;
         }
         if (reach_append_height()) {
@@ -131,13 +189,13 @@ var light_pager = function(site) {
     }
 
     var stop_scroll_listener = function() {
-        appending = false;
+        runtime.appending = false;
         window.removeEventListener("scroll", scroll_listener, false);
     };
 
     var control = {
         'start_paging' : function() {
-            site.appended_count = 0;
+            runtime.appended_count = 0;
             start_scroll_listener();
         },
         'continue_paging' : function() {
@@ -148,12 +206,15 @@ var light_pager = function(site) {
         }
     };
 
+    absolute_site_attrs();
     start_scroll_listener();
     return control;
 }
 
+/* rule setup functions */
+
 var select_site = function(sites) {
-    var url = document.URL;
+    var url = location.href;
     var i, j, reg, site, site_urls, site_url;
     for (i = 0; i < sites.length; i += 1) {
         site = sites[i];
@@ -173,17 +234,31 @@ var select_site = function(sites) {
     return null;
 };
 
-var site = select_site(SITES);
-if (site !== null) {
+var setup_site_default = function(site, site_default) {
     if (site.height === undefined) {
-        site.height = DEFAULT.height
+        site.height = site_default.height
     }
     if (site.count === undefined) {
-        site.count = DEFAULT.count;
+        site.count = site_default.count;
     }
-    control = light_pager(site);
+}
+
+var register_menus = function(control) {
     GM_registerMenuCommand("Start", control.start_paging, "s");
     GM_registerMenuCommand("Continue", control.continue_paging, "c");
     GM_registerMenuCommand("Stop", control.stop_paging, "t");
+}
+
+// only run as main file
+if (GM_info.script.name === 'Light Pager') {
+    var rule = JSON.parse(GM_getResourceText('rule'));
+    var site_default = rule["default"];
+    var sites = rule["sites"];
+    var site = select_site(sites);
+    if (site !== null) {
+        setup_site_default(site);
+        var control = light_pager(site);
+        register_menus(control);
+    }
 }
 
