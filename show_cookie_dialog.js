ce0cac55cdc85e48a7ddd98195f6f602aa60f8ee
muzuiget <muzuiget@gmail.com>
2013-02-15 02:25:05 +0800

    add show_cookie_dialog.js

---
+++ show_cookie_dialog.js
@@ -0,0 +1,41 @@
+/**
+ * Show cookie dialog.
+ */
+
+var xpcomFactory = {
+    windowMediator: function() {
+        return Cc['@mozilla.org/appshell/window-mediator;1']
+               .getService(Components.interfaces.nsIWindowMediator);
+    },
+    eTLDService: function() {
+        return Cc['@mozilla.org/network/effective-tld-service;1']
+               .getService(Components.interfaces.nsIEffectiveTLDService);
+    }
+};
+
+var xpcomUtil = {
+    getMostRecentWindow: function(name) {
+        var mediator = xpcomFactory.windowMediator();
+        return mediator.getMostRecentWindow(name);
+    },
+    getCurrentHost: function() {
+        var uri = content.document.documentURIObject;
+        var host;
+        try {
+            host = xpcomFactory.eTLDService().getBaseDomain(uri);
+        } catch (e) {
+            host = uri.asciiHost;
+        }
+        return host;
+    }
+};
+
+var cookieDialog = xpcomUtil.getMostRecentWindow('Browser:Cookies');
+var filterString = xpcomUtil.getCurrentHost();
+if (cookieDialog) {
+    cookieDialog.gCookiesWindow.setFilter(filterString);
+    cookieDialog.focus();
+} else {
+    openDialog('chrome://browser/content/preferences/cookies.xul',
+               'Browser:Cookies', '', {filterString: filterString});
+}
