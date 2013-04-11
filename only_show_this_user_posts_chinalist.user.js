a3c9ef52b08f7d8d944a8989be72d781d72c6152
muzuiget <muzuiget@gmail.com>
2013-04-12 04:04:18 +0800

    remove kdnet and mop

--- only_show_this_user_posts_chinalist.user.js
+++ only_show_this_user_posts_chinalist.user.js
@@ -3,11 +3,8 @@
 // @namespace   qixinglu.com
 // @description 内置中文网站的 Only Show This User Posts 规则
 // @require     https://raw.github.com/muzuiget/greasemonkey-scripts/4531270c3f8fe04cce9981251d1f925df0f93168/only_show_this_user_posts.user.js
-// @include     https://localhost/*
 // @include     http://www.douban.com/group/topic/*
 // @include     http://tieba.baidu.com/p/*
-// @include     http://club.kdnet.net/dispbbs.asp?*
-// @include     http://dzh.mop.com/whbm/*
 // ==/UserScript==
 
 var SITES = [
@@ -45,20 +42,6 @@ var SITES = [
     username: 'a.p_author_name, div.d_author_anonym',
     container: '<li class="ostup"></li>'
 },
-{
-    title: '凯迪论坛',
-    url: 'http://club.kdnet.net/dispbbs.asp?*',
-    post: 'div.posted-box-add, div.reply-box',
-    position: 'div.posts-control',
-    username: 'div.posted-info span.c-main a[target]'
-},
-{
-    title: '猫扑大杂烩',
-    url: 'http://dzh.mop.com/whbm/*',
-    post: '#body, div.main div.tzhfP',
-    position: '#lzxx_fun div.hfyc, li.caption div.hfyc',
-    username: '#lzxx_fun .ico_klz + a[target], li.caption a.name'
-}
 ];
 
 var site = select_site(SITES);
@@ -66,7 +49,7 @@ if (site !== null) {
     site.showOnlyLabel = '只看这个用户';
     site.showAllLabel = '显示全部用户';
     var control = ostup(site);
-    document.addEventListener("LightPagerAppended", function() {
+    document.addEventListener('LightPagerAppended', function() {
         control.add_buttons();
         control.do_filter();
     });
