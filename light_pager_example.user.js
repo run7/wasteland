f6e04c6079ee614fab3a2b2e4f5fa1c90e068abd
muzuiget <muzuiget@gmail.com>
2013-04-12 03:22:13 +0800

    add light_pager_example.user.js

---
+++ light_pager_example.user.js
@@ -0,0 +1,64 @@
+// ==UserScript==
+// @name        Light Pager Example
+// @namespace   qixinglu.com
+// @description A example for Light Pager
+// @grant       GM_xmlhttpRequest
+// @grant       GM_addStyle
+// @grant       GM_registerMenuCommand
+// @grant       GM_info
+// @require     https://github.com/muzuiget/greasemonkey-scripts/raw/20059c9b0909d4d39f741cdfe67939f29705dc09/light_pager.user.js
+// @include     http*://www.google.com/search?*
+// @include     https://addons.mozilla.org/*
+// @include     http://stackoverflow.com/questions*
+// ==/UserScript==
+
+var GLOBAL = {
+    separate: true,
+    loadingHTML: 'Loading：<a href="${url}">${current} / ${total}<a/>',
+    loadedHTML: 'Page：<a href="${url}">${current} / ${total}<a/>',
+    count: 9,
+    height: 0.9
+};
+var SITES = [
+{
+    title: 'Google Search',
+    url: 'http*://www.google.com/search?*',
+    next: '#pnnext',
+    content: '#center_col, #foot',
+    hidden: '#topstuff:not(.lp-first), ' +
+            '#extrares:not(.lp-first), ' +
+            '#foot:not(.lp-last) #xjs ~ *',
+    style: '.lp-sep { background-color: #EBEFF9; }',
+},
+{
+    title: 'Mozilla Addon',
+    url: 'https://addons.mozilla.org/*',
+    next: 'a.next',
+    content: 'div.island',
+    style: '.lp-sep {' +
+           '    background-color: #DEF2C6;' +
+           '    border-radius: 5px 5px 5px 5px;' +
+           '}',
+    separateInside: false
+},
+{
+    title: 'StackOverFlow',
+    url: 'http://stackoverflow.com/questions*',
+    next: 'a[rel="next"]',
+    content: '#questions'
+},
+];
+
+var register_menus_cn = function(control) {
+    GM_registerMenuCommand('Start', control.start_paging, 's');
+    GM_registerMenuCommand('Continue', control.continue_paging, 'c');
+    GM_registerMenuCommand('Stop', control.stop_paging, 't');
+};
+
+var site = select_site(SITES);
+if (site !== null) {
+    setup_site_global(site, GLOBAL);
+    var control = light_pager(site);
+    register_menus_cn(control);
+}
+
